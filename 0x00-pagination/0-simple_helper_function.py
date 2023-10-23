ad64c5bbd8eaf3f9875db78dcc1a441e2e6f5f49
Solomon Amsalu <solamsalu@gmail.com>
2023-10-23 17:02:28 +0300

    Task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 """
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
   start_index = (page-1) * page_size
   end_index = start_index + page_size
   return (start_index, end_index)
