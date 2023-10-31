9b12ae0f6f2b6b4836676cf76af3d847194d7429
Solomon Amsalu <solamsalu@gmail.com>
2023-10-31 22:28:54 +0300

    Task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def index() -> str:
+    """ The home/index page. """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
