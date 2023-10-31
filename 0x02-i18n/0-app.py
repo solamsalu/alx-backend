532d840f5d6ab93fcfdd64f94bc96a60639a38ca
Solomon Amsalu <solamsalu@gmail.com>
2023-10-31 22:20:53 +0300

    Task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,9 +2,12 @@
 """ Task 0: Basic Flask app """
 
 from flask import Flask, render_template
-app = Flask(name)
+app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
-    return render_template('index.html')
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
